80b609c120dabff5816447c8c8e0514099fbdb28
Win Tun <wintun.hlaing@outlook.com>
2024-02-21 22:00:47 +0630

    power shell log function

--- window-power-shell/log-write-function/write-log.ps1
+++ window-power-shell/log-write-function/write-log.ps1
@@ -1,10 +1,13 @@
 $logFile = "D:\bash-script-programming\power-shell-log\test.log"
 
 function writeLog {
-    Param ([string]$logStream)
+    param(
+        [Parameter(Mandatory = $true)][string] $message,
+        [Parameter(Mandatory = $false)] [ValidateSet("INFO","WARNING","ERROR")] [string] $level = "INFO"
+    )
     $timeStamp =(Get-Date).toString("yyy/MM/dd HH:mm:ss")
     $logMessage = "$timeStamp $logStream"
-    Add-Content $logFile -value $logMessage
+    Add-Content -Path $logFile -Value "$timestamp [$level] - $message"
 }
 
-writeLog "cron 1 is running."
+writeLog -message "cron 1 is running."
